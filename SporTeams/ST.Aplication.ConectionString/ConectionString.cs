092a283e79431e79fc1e348030986bb98cf6af49
Agustin Touceda Papa <agustin_touceda@hotmail.com>
2018-12-18 20:17:00 -0300

    Estructura completa terminada!

---
+++ SporTeams/ST.Aplication.ConectionString/ConectionString.cs
@@ -0,0 +1,13 @@
+﻿namespace ST.Aplication.ConectionString
+{
+    public static class ConectionString
+    {
+        const string BaseDatos = "Repositorio";
+
+        const string Servidor = @"DESKTOP-I9AA7DR\SQLEXPRESS";
+
+        public static string GetWithWindows => $"Data Source={Servidor}; " +
+                $"Initial Catalog={BaseDatos}; " +
+                $"Integrated Security = true;";
+    }
+}
---
+++ SporTeams/ST.Infrastructure.Context/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using static ST.Aplication.ConectionString.ConectionString;
+
+namespace ST.Infrastructure.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base(GetWithWindows)
+        {
+            Configuration.LazyLoadingEnabled = false;
+
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
